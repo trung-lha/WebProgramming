70858b3c17ee2a7425e5459263dadc309882d432
ThaiLuu <thailuu9928@gmail.com>
2020-12-09 20:37:21 +0700

    add dat lich

---
+++ resources/js/components/Datlich.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="datlich">
+      <div class="list">
+          <div v-for="(calendar,index) in calendars" :key = "index"
+          @click="calendar.selected = !calendar.selected"
+          :class=" {selected :calendar.selected} "
+          >
+          
+          <span>{{calendar.time}}</span>
+</div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      calendars: [{time: '7-8:30' ,selected :false}, {time:'8:30-10',selected :false}, {time: '10-11:30',selected :false}, {time: '12:30-2',selected :false}, {time: '2-3:30',selected :false}, {time: '3:30-5',selected :false}],
+    };
+  },
+};
+</script>> 
+<style scoped >
+.list div{
+    display: inline-block;
+    border: 1px solid #ebebeb;
+    padding: 4px 15px;
+    margin-right: 5px;
+    border-radius: 4px;
+}
+.list div.selected{
+    background-color: aqua;
+    color: white;
+
+}
+</style>
